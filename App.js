a5f0aa60cccab1435d8aeca6de15edf5376a43e6
Irshad Ahmad <irshad.ahmad.in@lavu.com>
2024-06-12 08:42:19 +0530

    React components and React Elements and js variables usage in React page

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement
-('h1', 
-{id: 'heading'}, 
-'Hello, World! From React'
+//React Component
+const HeadingComponent = () => <h1 className='heading' tabIndex={0}>Namaste React Heading</h1>
+ 
+//React Element
+const title = <h6>Namaste ReactJS</h6>;
+//React Component composition.
+//we can put react element inside the component.
+// we can put javascript expression inside the component.
+const BodyComponent = () => (
+  <div id="container">
+    {1000+200}
+    {title}
+    <HeadingComponent />
+    {HeadingComponent()}
+    <HeadingComponent></HeadingComponent>
+    <h2 className="body">Namaste React Body</h2>
+  </div>
 );
+
 root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<BodyComponent />);
